ce6f7327264116975202f818dfd9715dc407f2ec
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-01-28 23:01:51 +0400

    turn into the most basic PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,17 +43,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Quickest Blog using contentful`,
+        short_name: `Basic Blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/zeeshaan-logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    `gatsby-plugin-offline`
   ],
 }
